7af9a4a248ef50f3e7ddc0692eb955e539b9d9a5
Francis <francispham89@gmail.com>
2020-09-25 12:28:58 -0700

    Abstracting Animations - Toggle with Fade Feature: Implemented Animation for mount/unmount Component

--- src/Fade.js
+++ src/Fade.js
@@ -1,3 +1,14 @@
-const Fade = ({ children, isActive }) => isActive && children;
+import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
-export default Fade;
+const Fade = ({ children, isActive }) => <AnimatePresence>
+  {isActive && 
+  <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+  >{children}</motion.div>}
+  <h1>Francis</h1>
+</AnimatePresence>;
+
+export default Fade
