2bd67605199a1db3af5c18497e8d8849299c35eb
Francis <francispham89@gmail.com>
2020-09-29 20:08:28 -0700

    Draggable Drawer Feature: Implemeneted Animating when Open/Close The Draggable Drawer with Buttons

--- src/BlogPosts.js
+++ src/BlogPosts.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { motion } from "framer-motion";
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from "framer-motion";
 
 const BlogPosts = () => {
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <motion.div 
       className="App"
@@ -11,6 +13,33 @@ const BlogPosts = () => {
     >
       <main className="layout">
         <h3>Blog Posts</h3>
+        <button onClick={() => setIsActive(true)}>
+          Open Drawer
+        </button>
+        <AnimatePresence>
+          {isActive &&(
+            <motion.div
+              drag="y"
+              // This alow the Drawer to move back!
+              dragConstraints={{ top: 0, bottom: 0 }}
+              className="drawer-wrapper"
+            >
+              <motion.div
+                initial={{ opacity: 0.5, y: "110%" }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0.5, y: "110%" }}
+                // This slower the moving Drawer
+                transition={{ damping: 25, type: "spring" }}
+              >
+                <div className="drawer">
+                  <button onClick={() => setIsActive(false)}>
+                    Close Drawer
+                  </button>
+                </div>
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
         {posts.map(post => 
           <div className="card" key={post}>
               <h4>Post Number one</h4>
